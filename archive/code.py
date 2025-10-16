41059a4bb67abce65e0bd2e0366cb934ee1c7597
jmCabrillana <jean_manuel.cabrillana@yahoo.fr>
2025-10-16 15:57:24 +0000

    archive code

---
+++ archive/code.py
@@ -0,0 +1,10 @@
+# import cProfile, pstats
+
+# def run_profiled():
+#     run_rtrl(tt)  # call your entry function
+
+# with cProfile.Profile() as pr:
+#     run_profiled()
+
+# stats = pstats.Stats(pr)
+# stats.sort_stats(pstats.SortKey.CUMULATIVE).print_stats(30)
