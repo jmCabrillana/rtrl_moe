022cb59a5811ee9916ab1785008ab569d69b2128
jmCabrillana <jean_manuel.cabrillana@yahoo.fr>
2025-11-10 12:41:39 +0800

    update rtrl moe

--- rtrl_block.py
+++ rtrl_block.py
@@ -1,12 +1,10 @@
-import torch
-from structure import CircularTree, sparse_left_mul
-
+# Block RTRL
 # per-point function to batch jacobian later
-def make_f_single(model, proj=slice(None)):
+def make_f_single(model, write=slice(None)):
     def f(params, h, x, kw):
         x1, h1 = rearrange(x, '... -> 1 ...'), rearrange(h, '... -> 1 ...')
         *_, h_next_b = functional_call(model, params, (x1, h1), kw)
-        return rearrange(h_next_b, '1 h -> h')[proj] # remove fake batch, slice
+        return rearrange(h_next_b, '1 h -> h')[write] # remove fake batch, slice
     return f
 
 def add_grad_(p, g):
@@ -19,20 +17,22 @@ def add_grad_(p, g):
 
 class BlockRTRL:
     @torch.no_grad()
-    def __init__(self, state_params, B, H, len_buffer=64):
+    def __init__(self, state_params, B, H, len_buffer=64, len_buffer_hk=8):
         self.state_params = state_params
         self.P_t = {k:torch.zeros([B, H, p.numel()]).to(p) for k,p in self.state_params.items()}
         self.last_update = {k:0 for k in self.state_params.keys()}
         self.len_buffer = len_buffer
-        self.buffer = CircularMatTree(len_buffer, torch.eye(H).expand(B, -1, -1).to(list(self.state_params.values())[0]))
+        self.len_buffer_hk = len_buffer_hk
+        # self.buffer = CircularMatTree(len_buffer, torch.eye(H).expand(B, -1, -1).to(list(self.state_params.values())[0]))
+        self.buffer = CircularTree(len_buffer, None, sparse_left_mul)
         self.last_hk = [0]
         self.t = 0
 
     def reset(self):
         [P_t.zero_() for P_t in self.P_t.values()]
 
-    def get_left_product(self, k, l=None):
-        if l is None: l = len(self.buffer)
+    @torch.no_grad()
+    def get_left_product(self, k, l):
         return self.buffer.query(k, l)
       #   if l is None: l = len(self.buffer)
       #   L = torch.eye(H).expand(B, -1, -1)
@@ -40,23 +40,36 @@ class BlockRTRL:
       #     L = torch.bmm(self.buffer[i], L)
       #   return L
 
-    def step(self, model, x_t, h_t, loss, active_params, proj=slice(None), **kw):
+    @torch.no_grad()
+    def step(self, model, x_t, h_t, loss, active_params, read=None, write=None, **kw):
         """
         x_t: [B,...], h_t: [B,H], P_t: [B,H,Tp], dL_dH_t: [B,H]
         """
+        t0 = time.time()
         params = dict(model.named_parameters())
         B, H = h_t.shape[:2]
-        f1 = make_f_single(model, proj)
+        read = list(range(H)) if read is None else read
+        write = list(range(H)) if write is None else write
+        f1 = make_f_single(model, write)
+
+        print_time = False
 
         # batched jacobian of per-sample f
-        Jh = torch.eye(H).repeat(B, 1, 1).to(h_t)
-        Jh_proj = vmap(jacrev(f1, argnums=1), in_dims=(None, 0, 0, None))(active_params, h_t, x_t, kw)  # [B,H,H]
-        Jh[:, proj] = Jh_proj
-        Jtheta_proj = vmap(jacrev(f1, argnums=0), in_dims=(None, 0, 0, None))(active_params, h_t, x_t, kw) # [B,H,[...]]
-        Jtheta_proj = {k:rearrange(v, 'b h ... -> b h (...)') for k, v in Jtheta_proj.items()}  # [B,H,[Tp]]
+        with torch.enable_grad():
+            if print_time: t1 = time.time(); print("0: ", t1-t0); t0=t1
+            Jh_proj = vmap(jacrev(f1, argnums=1), in_dims=(None, 0, 0, None))(active_params, h_t, x_t, kw)  # [B,W,H]
+            # Jh = torch.eye(H).repeat(B, 1, 1).to(h_t), Jh[:, proj] = Jh_proj
+            if print_time: t1 = time.time(); print("1: ", t1-t0)
+            Jtheta_proj = vmap(jacrev(f1, argnums=0), in_dims=(None, 0, 0, None))(active_params, h_t, x_t, kw) # [B,W,[...]]
+            Jtheta_proj = {k:rearrange(v, 'b h ... -> b h (...)') for k, v in Jtheta_proj.items()}  # [B,W,[Tp]]
+            # >>> Detach before storing <<<
+            Jh_proj = Jh_proj.detach()
+            Jtheta_proj = {k: v.detach() for k, v in Jtheta_proj.items()}
 
         # Update circular buffer
-        self.buffer.update(Jh)
+        if print_time: t1 = time.time(); print("2: ", t1-t0); t0=t1
+        # self.buffer.update((proj, Jh_proj))
+        if print_time: t1 = time.time(); print("3: ", t1-t0); t0=t1
         # self.buffer.append(Jh); if len(self.buffer) > self.len_buffer: self.buffer.pop(0)
 
         # RTRL recursion on active or expiring sensitivities
@@ -65,29 +78,37 @@ class BlockRTRL:
             if k in active_params.keys():
                 # Jtheta = torch.zeros([B, H, self.state_params[k].numel()]); Jtheta[:, proj] = Jtheta_tree[k]
                 # t <-> index [q-1]; product for time s < t starts at s+1, ie index [s - t + (q-1) + 1] = [s - t + q]
-                if self.last_update[k] < self.t-1:
-                  L_Jh = self.get_left_product(self.last_update[k] - self.t + self.len_buffer, self.len_buffer-1)
-                  self.P_t[k] = torch.bmm(L_Jh, self.P_t[k])  # update to P_t-1 first
-                self.P_t[k][:, proj] = Jtheta_proj[k] + torch.bmm(Jh_proj, self.P_t[k]) # Optimize to update only on projected lines
+                # idx, L_Jh = self.get_left_product(self.last_update[k] - self.t + self.len_buffer, self.len_buffer)
+                # self.P_t[k][:, idx] = L_Jh @ self.P_t[k]
+                self.P_t[k][:, write] = Jh_proj[:,:,read] @ self.P_t[k][:, read]
+                self.P_t[k][:, write] += Jtheta_proj[k]
                 self.last_update[k] = self.t
             # Expiring parameters update
-            L_Jh = self.get_left_product(0)
-            if k not in active_params.keys() and self.last_update[k] <= self.t - self.len_buffer: # == in practice
-                self.P_t[k] = torch.bmm(L_Jh, self.P_t[k])
-                self.last_update[k] = self.t
+            else:
+                self.P_t[k][:, write] = Jh_proj[:,:,read] @ self.P_t[k][:, read]
+        # if print_time: t1 = time.time(); print("4.1: ", t1-t0); t0=t1
+        # for k in self.state_params.keys():
+        #     idx, L_Jh = self.get_left_product(0, self.len_buffer)
+        #     if k not in active_params.keys() and self.last_update[k] <= self.t - self.len_buffer: # == in practice
+        #         self.P_t[k][:, idx] = L_Jh @ self.P_t[k]
+        #         self.last_update[k] = self.t
+        # if print_time: t1 = time.time(); print("4.2: ", t1-t0); t0=t1
 
 
         # Backpropagate, Maintain last activated
-        if proj != slice(None):
-            I = list(set(self.last_hk) & set(proj))
-            self.last_hk = [k for k in self.last_hk + proj if k not in I] + I
         if loss is not None:
             dL_dTheta = dict(zip(params.keys(), torch.autograd.grad(loss, params.values(), retain_graph=True, allow_unused=True)))
-            (dL_dH_t,) = torch.autograd.grad(loss, h_t, retain_graph=True) #[:,self.last_hk]
+            (dL_dH_t,) = torch.autograd.grad(loss, h_t, retain_graph=False) #[:,self.last_hk]
             for k, g in dL_dTheta.items():
+                # if k not in self.state_params.keys():
                 add_grad_(params[k], g)
             for k, p in self.state_params.items():
-                g_mean = torch.einsum('b h, b h t -> b t', dL_dH_t, self.P_t[k]).mean(0)
+                g_mean = torch.einsum('b h, b h t -> b t', dL_dH_t, self.P_t[k]).mean(0) #[self.last_hk]
                 add_grad_(params[k], g_mean.view(p.shape))
+        if len(write) != H:
+            I = set(self.last_hk) & set(write)
+            self.last_hk = ([k for k in self.last_hk + write if k not in I] + list(I))
+            self.last_hk = self.last_hk[-self.len_buffer_hk:]
+        if print_time: t1 = time.time(); print("5: ", t1-t0); t0=t1
 
         self.t += 1
